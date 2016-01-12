8b7b8f512643874052f841825b0c20e37d523260
diao <yuanpengxiong@foxmail.com>
2016-01-12 19:47:56 +0800

    add weather test 2016/1/12

--- main/views.py
+++ main/views.py
@@ -7,7 +7,8 @@ import hashlib
 import os
 # python django 提供的良好处理中文的库
 from django.utils.encoding import smart_str
-from django.views.decorators.csrf import csrf_exempt  
+from django.views.decorators.csrf import csrf_exempt
+import sys, urllib, urllib2, json
 # 加在一个XML的解析库
 try:
     import xml.etree.cElementTree as ET
@@ -17,6 +18,7 @@ except ImportError:
 
 TOKEN = "diaoxiong"
 APP_ID = "wx45779b96e3d17b91"
+API_STORE_KEY = "7aa30ddb083f3f3c4c6e09bc084f57a4"
 #禁用DJANGo提供的跨站脚本攻击服务
 @csrf_exempt
 def process(request):
@@ -54,12 +56,13 @@ def reply(request):
 	xml = smart_str(request.body)
 	msg = parse_msg(xml)
 	#将获得的数据原样返回给服务器
+	content = weather(msg["Content"])
 	return render(request,'reply_text.xml',
 				{'toUser':msg["FromUserName"],
 				'fromUser':msg["ToUserName"],
 				'createTime':str(int(time.time())),
 				'msgType':msg["MsgType"],
-				'content':msg["Content"],
+				'content':content,
 				},
 				content_type = "application/xml")
 
@@ -71,3 +74,14 @@ def parse_msg(rawStr):
 		msg[child.tag] = child.text
 	return msg
 
+def weather(city):
+	global API_STORE_KEY
+	# 构造网址
+	url = 'http://apis.baidu.com/heweather/weather/free?city='+city
+	apikey = API_STORE_KEY
+	req = urllib2.Request(url)
+	req.add_header("apikey", apikey)
+	#利用API请求天气
+	resp = urllib2.urlopen(req)
+	content = resp.read()
+	return content
